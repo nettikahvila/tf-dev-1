5272537c0add736dbcf81239abd03a03bc2bc455
Matti Paksula <matti.paksula@iki.fi>
2023-06-25 11:01:34 +0300

    test remove

--- main.tf
+++ main.tf
@@ -6,25 +6,25 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
-resource "hcloud_firewall" "test_1" {
-  name = "test-1"
-  rule {
-    direction = "in"
-    protocol  = "icmp"
-    source_ips = [
-      "0.0.0.0/0",
-      "::/0"
-    ]
-  }
+# resource "hcloud_firewall" "test_1" {
+#   name = "test-1"
+#   rule {
+#     direction = "in"
+#     protocol  = "icmp"
+#     source_ips = [
+#       "0.0.0.0/0",
+#       "::/0"
+#     ]
+#   }
 
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "80-85"
-    source_ips = [
-      "0.0.0.0/0",
-      "::/0"
-    ]
-  }
+#   rule {
+#     direction = "in"
+#     protocol  = "tcp"
+#     port      = "80-85"
+#     source_ips = [
+#       "0.0.0.0/0",
+#       "::/0"
+#     ]
+#   }
 
-}
+# }
