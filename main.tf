878955fc0b5ab37e2845f183817df62e697c42bb
Matti Paksula <matti.paksula@iki.fi>
2023-06-25 10:51:37 +0300

    provider

---
+++ main.tf
@@ -0,0 +1,30 @@
+variable "hcloud_token" {
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_firewall" "test_1" {
+  name = "test-1"
+  rule {
+    direction = "in"
+    protocol  = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "80-85"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+}
